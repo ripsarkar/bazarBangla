aa6d5d58a9b712505c5a180b705a7ff7569c853d
ibm <ripsarka@in.ibm.com>
2016-02-15 12:57:31 +0530

    4245

--- WebContent/js/search.js
+++ WebContent/js/search.js
@@ -3158,7 +3158,7 @@ $scope.cliThreModMid = function($event,ndvlqe,nameval){
 
     $scope.ClikedResult = function(node) {
         //code for user specific Industry selection
-        if(($rootScope.role == "USER_VIEW" || $rootScope.role == "USER_EXPORT") && notpushed ==true){
+        if($rootScope.role == "USER_VIEW" || $rootScope.role == "USER_EXPORT"){
             var indus ={};
             indus["id"] = localStorage.getItem("industrySurrId");
             postjsonresult.Industry.push(indus);
