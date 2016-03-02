36e52d14771ab24f17bcd0f3268e1bf63ac1c619
ibm <ripsarka@in.ibm.com>
2016-03-02 15:01:21 +0530

    3742

--- WebContent/js/createnew.controller.js
+++ WebContent/js/createnew.controller.js
@@ -295,7 +295,16 @@ function($scope, $rootScope, $state, $http, UsecaseService) {
 
 
 $scope.chckindustry = function() {
+    Indtsyarray = [];
     if (typeof $scope.UsecaseIntry != 'undefined' && $scope.UsecaseIntry.length > 0) {
+
+        for (var i=0;i<$scope.UsecaseIntry.length-1;i++){
+            //alert($scope.UsecaseIntry[i]);
+            if($scope.UsecaseIntry[i] == $scope.UsecaseIntry[$scope.UsecaseIntry.length-1]){
+                $scope.UsecaseIntry.splice(-1);
+            }
+        }
+
         for (var i = 0; i < $scope.UsecaseIntry.length; i++) {
             var Indtsy = {};
             Indtsy.surrId = $scope.UsecaseIntry[i];
@@ -1803,7 +1812,15 @@ app.controller("UpdateusecaseController", ["$scope", "$rootScope", "$state", "$h
                 }
 
                 $scope.chckindustry = function() {
+                    Indtsyarray = [];
+
                     if (typeof $scope.UsecaseIntry != 'undefined' && $scope.UsecaseIntry.length > 0) {
+            
+                    for (var i=0;i<$scope.UsecaseIntry.length-1;i++){
+                      if($scope.UsecaseIntry[i] == $scope.UsecaseIntry[$scope.UsecaseIntry.length-1]){
+                        $scope.UsecaseIntry.splice(-1);
+                      }
+                    }
                         for (var i = 0; i < $scope.UsecaseIntry.length; i++) {
                             var Indtsy = {};
                             Indtsy.surrId = $scope.UsecaseIntry[i];
