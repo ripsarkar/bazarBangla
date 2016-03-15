d299ac82bace48116b5804cec2243c49985d65bd
ibm <ripsarka@in.ibm.com>
2016-03-15 19:57:24 +0530

    4960

--- WebContent/js/permissions.js
+++ WebContent/js/permissions.js
@@ -155,7 +155,7 @@ $scope.sendPermissions = function(){
                   data:JSON.stringify(postjson)
                 }
                 $http(postdata).success(function(result){
-                          $http.get($rootScope.url + "/managePermission/" + $rootScope.user_name + '/' + $rootScope.companyNamee).success(function(result) {
+                          $http.get($rootScope.url + "/managePermission/" + $rootScope.user_name + '/' + $rootScope.userIndustChVa).success(function(result) {
                               sessionStorage.setItem("fetchPermission", JSON.stringify(result));
                               $scope.permission = sessionStorage.getItem("fetchPermission");
                              // console.log(sessionStorage.getItem("fetchPermission"));
