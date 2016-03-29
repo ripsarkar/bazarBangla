73bc68bdc15c27a79cdea2cfab01f39243b62498
soumensardar <soumen.sardar@in.ibm.com>
2016-03-29 17:46:12 +0530

    z

--- WebContent/js/home.controller.js
+++ WebContent/js/home.controller.js
@@ -24,14 +24,14 @@ function HomeController(UserService,UserAuthFactory,AuthenticationFactory, $root
 	   	         
 	   	             
 	   	        }).error(function(data, status, headers, config) {
-	   	          //alert('Oops something went wrong!');
-			   	       if (status == 403) {
+	   	          alert('There could be some temporary technical problem. Please refresh / try again  and contact your system administrator');
+			   	       /*if (status == 403) {
 		           		//alert(data);
 		           		var r = confirm(data);
 		           		if (r == true) {
 		           			$scope.localStorageclear();
 		           		 }
-			   	       }
+			   	       }*/
 		           	
 	   	        });
 	   	   
