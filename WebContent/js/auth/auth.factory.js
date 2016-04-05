d81afb195a204b2a4db1b75bff4da33e5011891a
ibm <ripsarka@in.ibm.com>
2016-04-05 12:09:37 +0530

    5166

--- WebContent/js/auth/auth.factory.js
+++ WebContent/js/auth/auth.factory.js
@@ -65,10 +65,8 @@
         'responseError': function(errorResponse) {
             switch (errorResponse.status) {
             case 403:
-                $window.location = './403.html';
-                break;
-            case 500:
-                $window.location = './500.html';
+                $location.path('/login');
+                alert("You are logged in from another instance");
                 break;
             }
             return $q.reject(errorResponse);
