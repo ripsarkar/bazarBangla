89ceb60ee45b85bedeab59de28169799aef29e49
ibm <ripsarka@in.ibm.com>
2016-04-20 11:56:17 +0530

    encrypt decrypt

--- WebContent/js/home.controller.js
+++ WebContent/js/home.controller.js
@@ -24,11 +24,12 @@ function(event, toState, toParams, fromState, fromParams){
 		usernameId = window.location.href;
 		});
 		var stringForUsername = usernameId.split("&");
-		var mj = stringForUsername[1].split("=");
-		/*var loginPik = mj[1];
+		var mj = stringForUsername[1].split("nU=");
+		var loginPik = mj[1];
+		var dec = decodeURIComponent(loginPik);
 		alert(mj[1]);
-		mj[1] = Aes.Ctr.decrypt(loginPik, "rip", 256);
-		alert(mj[1]);*/
+		mj[1] = Aes.Ctr.decrypt(dec, "rip", 256);
+		alert(mj[1]);
 		//token for session
 		var tokenForSession = stringForUsername[0].split("SESSION_ID=");
 		console.log(tokenForSession[1]);
