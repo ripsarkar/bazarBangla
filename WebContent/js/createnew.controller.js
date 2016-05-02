75fc0b21ad0c9e4576fb67f0a5665cceef03cf3c
soumensardar <soumen.sardar@in.ibm.com>
2016-05-02 15:57:52 +0530

    sadadas

--- WebContent/js/createnew.controller.js
+++ WebContent/js/createnew.controller.js
@@ -79,15 +79,7 @@ app.service("UsecaseService", function() {
 app.controller("UsecaseCtrlController", ["$scope", "$rootScope", "$state", "$http", 'UsecaseService',
 function($scope, $rootScope, $state, $http, UsecaseService) {
 
-	 $scope.roles = [
-	                 'guest', 
-	                 'user', 
-	                 'customer', 
-	                 'admin'
-	               ];
-	               $scope.user = {
-	                 roles: ['user']
-	               };
+
     $scope.pagemain = {
         main: true,
         usecase: false,
