6d4e3ecd1ac29eb66504a2020bd141711343288c
ibm <ripsarka@in.ibm.com>
2016-05-03 23:09:17 +0530

    1234

--- WebContent/js/permissions.js
+++ WebContent/js/permissions.js
@@ -12,6 +12,7 @@ app.controller("permissionsCtrl",["$scope","$http", "$rootScope","$q","$timeout"
 		$scope.dummyMembertabOrga = true;
       }
   });
+  $scope.roleListDisPlay = true;
       if($rootScope.tabName == "Role"){
 		$scope.roleListDisPlay = false;
       }
