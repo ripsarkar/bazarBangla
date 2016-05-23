e4aadaaadf43faad8a4df45ab51b59e77833ae83
ibm <ripsarka@in.ibm.com>
2016-05-23 14:51:12 +0530

    5447

--- WebContent/js/createnew.controller.js
+++ WebContent/js/createnew.controller.js
@@ -3323,20 +3323,26 @@ app.controller("UpdateusecaseController", ["$scope", "$rootScope", "$state", "$h
                                 if($scope.dataeventAttr2[j].CUSTOM_EVENT_OBJ == "Yes"){
                                     for (var k = 0; k < $scope.crtRuleEventAttribute.length; k++) {
                                         if ($scope.dataeventAttr2[j].RULE_ATT_SURR_ID == $scope.crtRuleEventAttribute[k]) {
+                                            customvalueYes = {};
                                             customvalueYes.SurrId = $scope.dataeventAttr2[j].RULE_ATT_SURR_ID;
                                             customvalueYes.Selected = "Yes";
                                             custonEVattArr.push(customvalueYes);
+                                            $scope.dataeventAttr2.splice(j,1);
                                             flagToCus = true;
+                                            noFlagToCus = true;
                                             break;
                                         }
                                     }
                                     if(flagToCus == false){
+                                            customvalueYes = {};
                                             customvalueYes.SurrId = $scope.dataeventAttr2[j].RULE_ATT_SURR_ID;
                                             customvalueYes.Selected = "No";
                                             custonEVattArr.push(customvalueYes);
+                                            $scope.dataeventAttr2.splice(j,1);
                                             noFlagToCus = true;
                                             break;
                                     }
+                                    j=-1;
                                 }
                             }
                             if(noFlagToCus == false){
